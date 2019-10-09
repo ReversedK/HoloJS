6b434845f0b86e6e6a8426cbc2c20ecbd8423669
ReversedK <reversed.k@gmail.com>
2019-10-09 19:09:23 +0000

    changed link identifiers in lib.rs refactored holojs lib

--- js/holojs.js
+++ js/holojs.js
@@ -2,13 +2,17 @@
 const xhr = require('superagent');
 
 class HoloJs {
-    constructor(collection_name,config) { 
-      this.collection_name = collection_name;     
-      this.instance_name = config.instance_name; 
-      this.conductor_endpoint = config.conductor_endpoint;   
-    }  
+    constructor(collection,config) {
+      this.collection_name = collection.name;
+      this.collection_addr = collection.addr;
+      this.instance_name = config.instance_name;
+      this.conductor_endpoint = config.conductor_endpoint;
+    }
     async setup() {
-      this.collection_addr = await this.create_collection(this.collection_name);       
+      console.log('setting up collection '+this.collection_name);
+      //this.collection_addr = await this.create_collection(this.collection_name);
+      console.log('done setting up collection '+this.collection_name+': #',this.collection_addr);
+        return true;
   }
     preparePayload(zome,fn,payload_obj) {
        return {
@@ -25,17 +29,20 @@ class HoloJs {
     }
 
     async callHoloInstance(zome,fn,payload,callback=false) {
+    //  console.log("*******************",this)
         let response;
-        payload = this.preparePayload(zome,fn,payload);       
-        
-        if(typeof callback == 'function') 
+        payload = this.preparePayload(zome,fn,payload);
+
+        if(typeof callback == 'function')
             xhr.post(this.conductor_endpoint).set('Content-Type', 'application/json').set('accept', 'json')
-            .send(payload) 
+            .send(payload)
             .end((err, res) => {
              callback(err,res.body);
         }); else {
             response = await xhr.post(this.conductor_endpoint).set('Content-Type', 'application/json').set('accept', 'json').send(payload);
-            try {                
+
+            try {
+              console.log(fn,payload,response.body.result);
             return JSON.parse(response.body.result).Ok
             } catch(e) { console.log(e); console.log(response.body);return e;}
         }
@@ -44,73 +51,74 @@ class HoloJs {
     /************** ***************/
     /* CRUD methods & some more...*/
     /*************************** */
-    async add(item2add) { 
-        item2add.entityType = this.collection_name;       
-        item2add = {  entityType: item2add.entityType, item: JSON.stringify(item2add) }      
+    async add(item2add) {
+      console.log('adding item to collection '+this.collection_name+' #',this.collection_addr);
+        item2add.entity_type = this.collection_name;
+        item2add = {  entity_type: item2add.entity_type, item: JSON.stringify(item2add) }
         let payload = { item: item2add, base_addr: this.collection_addr };
-        return await this.callHoloInstance('collections', 'add_item',payload);  
+        return await this.callHoloInstance('collections', 'add_item',payload);
     };
 
-    async update(new_item,item_addr) { 
-        new_item.entityType = this.collection_name;         
-        new_item = {  entityType: this.collection_name, item: JSON.stringify(new_item) }      
+    async update(new_item,item_addr) {
+        new_item.entity_type = this.collection_name;
+        new_item = {  entity_type: this.collection_name, item: JSON.stringify(new_item) }
         let payload = { new_entry: new_item, item_address: item_addr };
-        return await this.callHoloInstance('collections', 'update_item',payload);  
+        return await this.callHoloInstance('collections', 'update_item',payload);
     };
 
-    async delete(item_addr) {       
+    async delete(item_addr) {
         let payload = {item_address: item_addr };
-        return await this.callHoloInstance('collections', 'delete_item',payload);  
+        return await this.callHoloInstance('collections', 'delete_item',payload);
     };
 
     async create_collection(name) {
-        let payload = { "collection": { "name": name } };       
-        return await this.callHoloInstance('collections', 'create_collection',payload);  
-    };      
+        let payload = { "collection": { "name": name } };
+        return await this.callHoloInstance('collections', 'create_collection',payload);
+    };
 
     async find(search={}) {
-        let payload = { 
+        let payload = {
             collection_addr: this.collection_addr,
             link_tag : this.collection_name,
             search : JSON.stringify(search)
           };
-        return await this.callHoloInstance('collections', 'get_list',payload);  
+        return await this.callHoloInstance('collections', 'get_list',payload);
     };
 
     async findLinkedItems(base_addr,link_tag,search={}) {
-        let payload = { 
+        let payload = {
             item_addr: base_addr,
             link_tag : link_tag,
             search : JSON.stringify(search)
           };
-        return await this.callHoloInstance('collections', 'get_linked_items',payload);  
+        return await this.callHoloInstance('collections', 'get_linked_items',payload);
     };
 
     async link(item_address,linkto_address, link_tag){
-        let payload = { 
+        let payload = {
             item_address: item_address,
             link_tag : link_tag,
             linkto_address : linkto_address
           };
-        return await this.callHoloInstance('collections', 'link_items',payload);  
+        return await this.callHoloInstance('collections', 'link_items',payload);
   }
 
   async unlink(base_addr,target, link_tag){
-    let payload = { 
+    let payload = {
         base_item: base_addr,
         link_tag : link_tag,
         target : target
       };
-    return await this.callHoloInstance('collections', 'unlink_items',payload);  
+    return await this.callHoloInstance('collections', 'unlink_items',payload);
 }
   async link_bidirectional(item_address,linkto_address, link_tag_ab,link_tag_ba){
-    let payload = { 
+    let payload = {
         item_a: item_address,
         link_tag_ab : link_tag_ab,
         link_tag_ba : link_tag_ba,
         item_b : linkto_address
       };
-    return await this.callHoloInstance('collections', 'link_bidir',payload);  
+    return await this.callHoloInstance('collections', 'link_bidir',payload);
 }
 }
 
--- zomes/collections/code/src/lib.rs
+++ zomes/collections/code/src/lib.rs
@@ -1,29 +1,36 @@
-#![feature(try_from)]
 #[macro_use]
 extern crate hdk;
+extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-
+extern crate serde_json;
 #[macro_use]
-extern crate holochain_core_types_derive;
+extern crate holochain_json_derive;
 
-use hdk::utils;
+
+
 use hdk::{
     error::ZomeApiResult,
     holochain_core_types::{
         link::LinkMatch,
-        hash::HashString,
-        error::HolochainError,
         dna::entry_types::Sharing,
-        json::JsonString,
-        cas::content::Address,
         entry::Entry,
-        
+
     }
 };
-use serde_json;
+
+use hdk::holochain_persistence_api::{
+    cas::content::Address,
+};
+
+
 use serde_json::value::Value;
- 
+
+use hdk::holochain_json_api::{
+    error::JsonError,
+    json::JsonString,
+};
+
 define_zome! {
     entries: [
         entry!(
@@ -52,11 +59,11 @@ define_zome! {
             validation_package: || hdk::ValidationPackageDefinition::Entry,
             validation: |validation_data: hdk::EntryValidationData<HoloJsEntry>| {
                 Ok(())
-            }, 
+            },
             links: [
                 to!(
                     "HoloJsEntry",
-                    link_type: "items",
+                    link_type: "related_items",
                     validation_package: || hdk::ValidationPackageDefinition::Entry,
                     validation: |_validation_data: hdk::LinkValidationData| {
                         Ok(())
@@ -65,11 +72,13 @@ define_zome! {
             ]
         )
     ]
- 
-    genesis: || {
+
+    init: || { Ok(()) }
+
+    validate_agent: |validation_data : EntryValidationData::<AgentId>| {
         Ok(())
     }
- 
+
 	functions: [
         create_collection: {
             inputs: |collection: Collection|,
@@ -77,42 +86,42 @@ define_zome! {
             handler: handle_create_collection
         }
         add_item: {
-            inputs: |item: HoloJsEntry, base_addr: HashString|,
+            inputs: |item: HoloJsEntry, base_addr: Address|,
             outputs: |result: ZomeApiResult<Address>|,
             handler: handle_add_item
         }
         get_list: {
-            inputs: |collection_addr: HashString, link_tag: String,search: JsonString|,
+            inputs: |collection_addr: Address, link_tag: String,search: JsonString|,
             outputs: |result: ZomeApiResult<GetCollectionResponse>|,
             handler: handle_get_list
         }
         get_linked_items: {
-            inputs: |item_addr: HashString, link_tag: String,search: JsonString|,
+            inputs: |item_addr: Address, link_tag: String,search: JsonString|,
             outputs: |result: ZomeApiResult<GetCollectionResponse>|,
             handler: handle_get_linked_items
         }
         link_items: {
-            inputs: |item_address: HashString,linkto_address: HashString, link_tag: String|,
+            inputs: |item_address: Address,linkto_address: Address, link_tag: String|,
             outputs: |result: ZomeApiResult<Address>|,
             handler: handle_link_items
         }
         unlink_items: {
-            inputs: |target: HashString,base_item: HashString, link_tag: String|,
+            inputs: |target: Address,base_item: Address, link_tag: String|,
             outputs: |result: ZomeApiResult<bool>|,
             handler: handle_unlink_items
         }
         update_item: {
-            inputs: |new_entry: HoloJsEntry,item_address: HashString|,
+            inputs: |new_entry: HoloJsEntry,item_address: Address|,
             outputs: |result: ZomeApiResult<Address>|,
             handler: handle_update_item
         }
         delete_item: {
-            inputs: |item_address: HashString|,
+            inputs: |item_address: Address|,
             outputs: |result: ZomeApiResult<bool>|,
             handler: handle_delete_item
-        }       
+        }
         link_bidir: {
-            inputs: |item_a: HashString,item_b: HashString, link_tag_ab: String,link_tag_ba: String|,
+            inputs: |item_a: Address,item_b: Address, link_tag_ab: String,link_tag_ba: String|,
             outputs: |result: ZomeApiResult<bool>|,
             handler: handle_link_bidir
         }
@@ -120,7 +129,7 @@ define_zome! {
     traits: {
         hc_public [get_linked_items,create_collection, add_item, get_list,link_bidir,delete_item,update_item,unlink_items,link_items]
     }
-}     
+}
 
 
 /******************************************* */
@@ -136,20 +145,20 @@ struct Collection {
 
 #[derive(Serialize, Deserialize, Debug, Clone, DefaultJson,PartialEq)]
 struct GetHoloJsEntry {
-    entityType: String,
+    entity_type: String,
     item: JsonString,
-    addr : HashString
+    addr : Address
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, DefaultJson,PartialEq)]
 struct HoloJsEntry {
-    entityType: String,
+    entity_type: String,
     item: JsonString
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, DefaultJson)]
-struct SearchObject {    
-    entityType: String,
+struct SearchObject {
+    entity_type: String,
     item: JsonString
 }
 
@@ -164,12 +173,12 @@ struct GetCollectionResponse {
 /******************************************** */
 
 fn handle_link_bidir(item_a: Address, item_b: Address,link_tag_ab:String,link_tag_ba:String) -> ZomeApiResult<bool> {
-        hdk::utils::link_entries_bidir(&item_a, &item_b, "items","items", &link_tag_ab, &link_tag_ba)?;
+        hdk::utils::link_entries_bidir(&item_a, &item_b, "related_items","related_items", &link_tag_ab, &link_tag_ba)?;
         Ok(true)
 }
 
-fn handle_link_items(item_address:HashString,linkto_address:HashString,link_tag:String)->ZomeApiResult<Address>{
-    let addr = hdk::api::link_entries( &item_address,&linkto_address, "items",&link_tag)?;
+fn handle_link_items(item_address:Address,linkto_address:Address,link_tag:String)->ZomeApiResult<Address>{
+    let addr = hdk::api::link_entries( &item_address,&linkto_address, "related_items",&link_tag)?;
 	Ok(addr)
 }
 
@@ -179,7 +188,7 @@ fn handle_delete_item(item_address: Address) -> ZomeApiResult<bool> {
 }
 
 fn handle_unlink_items(target:Address,base_item:Address,link_tag:String)-> ZomeApiResult<bool>{
-  hdk::remove_link(&base_item,&target,"items",&link_tag)?;
+  hdk::remove_link(&base_item,&target,"related_items",&link_tag)?;
   Ok(true)
 }
 
@@ -189,7 +198,7 @@ fn  handle_update_item(new_entry: HoloJsEntry, address: Address) -> ZomeApiResul
         Entry::App("HoloJsEntry".into(),new_entry.into()),
         &address.clone()
     )?;
-    Ok(address)    
+    Ok(address)
 }
 
 fn handle_create_collection(collection: Collection) -> ZomeApiResult<Address> {
@@ -202,39 +211,39 @@ fn handle_create_collection(collection: Collection) -> ZomeApiResult<Address> {
 	hdk::commit_entry(&list_entry)
 }
 
-fn handle_add_item(item: HoloJsEntry, base_addr: HashString) -> ZomeApiResult<Address> {
+fn handle_add_item(item: HoloJsEntry, base_addr: Address) -> ZomeApiResult<Address> {
     let clone_item:HoloJsEntry = item.clone();
     // define the entry
     let item = Entry::App(
         "HoloJsEntry".into(),
         item.into()
     );
-    
+
 	let item_addr = hdk::commit_entry(&item)?; // commit the collection item
-	hdk::link_entries(&base_addr, &item_addr, "items",&clone_item.entityType)?; // if successful, link to collection address
+	hdk::link_entries(&base_addr, &item_addr, "items",&clone_item.entity_type)?; // if successful, link to collection address
 	Ok(item_addr)
 }
 
 
-fn handle_get_list(collection_addr: HashString,link_tag: String,search:JsonString) -> ZomeApiResult<GetCollectionResponse> {
+fn handle_get_list(collection_addr: Address,link_tag: String,search:JsonString) -> ZomeApiResult<GetCollectionResponse> {
     // load the collection entry. Early return error if it cannot load or is wrong type
-    let collection = hdk::utils::get_as_type::<Collection>(collection_addr.clone())?;
-   
+     hdk::utils::get_as_type::<Collection>(collection_addr.clone())?;
+
     // try and load the collection items, filter out errors and collect in a vector
     let holojs_items : Vec<GetHoloJsEntry>  = hdk::get_links(&collection_addr, LinkMatch::Exactly("items"),LinkMatch::Exactly(&link_tag))?.addresses()
         .iter()
-        .map(|item_address|-> ZomeApiResult<GetHoloJsEntry>  {            
+        .map(|item_address|-> ZomeApiResult<GetHoloJsEntry>  {
            let the_entry = hdk::utils::get_as_type::<HoloJsEntry>(item_address.to_owned())?;
-           Ok(GetHoloJsEntry { addr : item_address.clone(), item: the_entry.item,entityType:the_entry.entityType})          
+           Ok(GetHoloJsEntry { addr : item_address.clone(), item: the_entry.item,entity_type:the_entry.entity_type})
         })
-        .filter_map(Result::ok)    
+        .filter_map(Result::ok)
         .collect::<Vec<GetHoloJsEntry>>();
 
     // filter the results
-        let curated_list: Vec<GetHoloJsEntry> = 
+        let curated_list: Vec<GetHoloJsEntry> =
         holojs_items.into_iter()
         .filter(|item| search_something(search.clone(), &item.clone()))
-        .collect::<Vec<GetHoloJsEntry>>(); 
+        .collect::<Vec<GetHoloJsEntry>>();
     //  then return the collection items
     Ok(GetCollectionResponse{
         name: link_tag.clone(),
@@ -242,25 +251,25 @@ fn handle_get_list(collection_addr: HashString,link_tag: String,search:JsonStrin
     })
 }
 
-fn handle_get_linked_items(item_addr: HashString,link_tag: String,search:JsonString) -> ZomeApiResult<GetCollectionResponse> {
+fn handle_get_linked_items(item_addr: Address,link_tag: String,search:JsonString) -> ZomeApiResult<GetCollectionResponse> {
     // load the  entry. Early return error if it cannot load or is wrong type
-    let base_item = hdk::utils::get_as_type::<HoloJsEntry>(item_addr.clone())?;
-   
+    hdk::utils::get_as_type::<HoloJsEntry>(item_addr.clone())?;
+
     // try and load the  items linked to base_item, filter out errors and collect in a vector
-    let holojs_items = hdk::get_links(&item_addr, LinkMatch::Exactly("items"),LinkMatch::Exactly(&link_tag))?.addresses()
+    let holojs_items = hdk::get_links(&item_addr, LinkMatch::Exactly("related_items"),LinkMatch::Exactly(&link_tag))?.addresses()
         .iter()
-        .map(|item_address|-> ZomeApiResult<GetHoloJsEntry>  {            
+        .map(|item_address|-> ZomeApiResult<GetHoloJsEntry>  {
            let the_entry = hdk::utils::get_as_type::<HoloJsEntry>(item_address.to_owned())?;
-           Ok(GetHoloJsEntry { addr : item_address.to_owned(), item: the_entry.item,entityType:the_entry.entityType})          
+           Ok(GetHoloJsEntry { addr : item_address.to_owned(), item: the_entry.item,entity_type:the_entry.entity_type})
         })
-        .filter_map(Result::ok)    
+        .filter_map(Result::ok)
         .collect::<Vec<GetHoloJsEntry>>();
 
     // filter the results
-        let curated_list: Vec<GetHoloJsEntry> = 
+        let curated_list: Vec<GetHoloJsEntry> =
         holojs_items.into_iter()
         .filter(|item| search_something(search.clone(), item))
-        .collect(); 
+        .collect();
     //  then return the  items encapsulated in a GetCollectionResponse struct
     Ok(GetCollectionResponse{
         name: link_tag.clone(),
@@ -276,8 +285,8 @@ fn handle_get_linked_items(item_addr: HashString,link_tag: String,search:JsonStr
 
 
 
-fn evaluateIsOperator(operator:String,searchval:Value,e:Value)->bool{
-    
+fn evaluate_is_operator(operator:String,searchval:Value,e:Value)->bool{
+
     if &operator=="is" {
     if searchval.is_u64() { e.as_u64()==searchval.as_u64() }
     else if searchval.is_i64() { e.as_i64()==searchval.as_i64() }
@@ -312,42 +321,42 @@ fn evaluateIsOperator(operator:String,searchval:Value,e:Value)->bool{
     else {false}
     } else {false}
 
-      /*  searchval.is_f64()=>Ok(e[&key].as_f64()==value2.as_f64()), 
-        searchval.is_i64()=>Ok(e[&key].as_i64()==value2.as_i64()), 
+      /*  searchval.is_f64()=>Ok(e[&key].as_f64()==value2.as_f64()),
+        searchval.is_i64()=>Ok(e[&key].as_i64()==value2.as_i64()),
         _ => ,Ok(e.as_str()==v.as_str())*/
-    
+
 }
 
 //TODO : arrays de conditions
-// TODO : search mode AND / OR 
+// TODO : search mode AND / OR
 // TODO  operateur regexp
 
-fn search_something(_search:JsonString,_item:&GetHoloJsEntry)->bool {    
+fn search_something(_search:JsonString,_item:&GetHoloJsEntry)->bool {
     let s:Value= serde_json::from_str(&_search.to_string()).unwrap();
-    let e:Value= serde_json::from_str(&_item.item.to_string()).unwrap(); 
+    let e:Value= serde_json::from_str(&_item.item.to_string()).unwrap();
     let mut res: bool = true;
     let search_obj = s.as_object().unwrap();
     //let foo = obj.get("item").unwrap();
-    for (operator, search_object_json) in search_obj.iter() {  
+    for (operator, search_object_json) in search_obj.iter() {
         if operator=="or" { res = false; } // if operator is or we start at false
         let search_object = search_object_json.as_object().unwrap();
          for (field, search_object_json_1) in search_object.iter() {
             let search_object2 = search_object_json_1.as_object().unwrap();
             for (key2, search_object_json_3) in search_object2.iter() {
-                let conditionArray = search_object_json_3.as_array().unwrap();
-                for (searchvalue) in conditionArray.iter() {
+                let condition_array = search_object_json_3.as_array().unwrap();
+                for searchvalue in condition_array.iter() {
             //  must always be an array of conditions
-            let estimation: Result<bool,String> = match key2.as_str() {        
+            let estimation: Result<bool,String> = match key2.as_str() {
             "contains" => Ok(e[&field].as_str().unwrap().contains(searchvalue.as_str().unwrap())),
-            "does_not_contain" => Ok(!e[&field].as_str().unwrap().contains(searchvalue.as_str().unwrap())),       
-            "is" | "is_more_than" | "is_less_than" | "is_not" | "more_or_equal_than" | "less_or_equal_than" => Ok(evaluateIsOperator(key2.to_string(),searchvalue.clone(), e[&field].clone())),  
+            "does_not_contain" => Ok(!e[&field].as_str().unwrap().contains(searchvalue.as_str().unwrap())),
+            "is" | "is_more_than" | "is_less_than" | "is_not" | "more_or_equal_than" | "less_or_equal_than" => Ok(evaluate_is_operator(key2.to_string(),searchvalue.clone(), e[&field].clone())),
             _ => Err("error".to_string())
                 };
                 if estimation.is_ok() {
                     if operator=="and" && estimation.clone().ok().unwrap()==false {  // this is an AND query : if anything is false, it's a no
-                        res=estimation.ok().unwrap(); 
+                        res=estimation.ok().unwrap();
                     } else if operator=="or" && estimation.clone().ok().unwrap()==true {  // this is an OR query : if anything is true, it's a go
-                        res=estimation.ok().unwrap(); 
+                        res=estimation.ok().unwrap();
                     }
                 }
             }
